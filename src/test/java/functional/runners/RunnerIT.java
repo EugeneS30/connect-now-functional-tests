6b4fb1420b5d7e8ca21db7e10549e415db2da262
Eugene Shragovich <shragovich@gmail.com>
2016-10-27 16:41:28 +0800

    added ignore tags to runner cucumber options

--- src/test/java/functional/runners/RunnerIT.java
+++ src/test/java/functional/runners/RunnerIT.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "classpath:connect_now_features" }, glue = { "functional.steps", "functional.dev",
 		"functional.configuration" }, plugin = { "html:target/cucumber-html-report",
-				"json:target/cucumber-json-report.json"})
+				"json:target/cucumber-json-report.json"}, tags = {"~@ignore, ~@skip"})
 public class RunnerIT {
 
 }
